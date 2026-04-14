0d27fc8a63c3706c826f276f7ee7b8a58200b96e
Edzon22 <rafasedju22@gmail.com>
2026-04-14 10:33:44 -0400

    HU-06: Implementacion de Login y Restricción de Acceso
    Se configuró la autenticación de usuarios, protección del dashboard mediante login_required y creación de la interfaz visual en templates.

--- geobolivia-dashboard/dashboard/views.py
+++ geobolivia-dashboard/dashboard/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .models import IndicadorClimatico
 
-
+# 1. VISTA DEL DASHBOARD (Restringida)
+@login_required
 def dashboard(request):
+    # Ahora 'acceso_publico' es False porque requiere estar logueado
     return render(request, 'dashboard.html', {
-        'acceso_publico': True
+        'acceso_publico': False
     })
 
+# 2. VISTA DE LOGIN (Nueva para tu tarea)
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            return render(request, 'login.html', {'error': 'Credenciales incorrectas'})
+            
+    return render(request, 'login.html')
 
+# 3. API DE INDICADORES (Lo que ya existía)
 def api_indicadores(request):
     categoria = request.GET.get('categoria', 'agropecuario')
     datos = IndicadorClimatico.objects.filter(categoria=categoria)
